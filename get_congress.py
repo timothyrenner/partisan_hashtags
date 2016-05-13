112a8b0cdd2c4938b8c920a65eac1c8e5e8af80b
Timothy Renner <renner.timothy@gmail.com>
2016-05-12 22:24:26 -0500

    Implemented fetching congress tweet IDs from sunlight API.

---
+++ get_congress.py
@@ -0,0 +1,13 @@
+from sunlight import congress
+from csv import DictWriter
+
+leg = congress.all_legislators_in_office()
+
+handles = [{'party': x['party'], 'twitter': x['twitter_id']} 
+		   for x in leg if 'twitter_id' in x and
+						   x['twitter_id'] is not None]
+
+with open('congress.csv','w') as f:
+	writer = DictWriter(f, fieldnames = ['party', 'twitter'])
+	for x in handles:
+		writer.writerow(x)
