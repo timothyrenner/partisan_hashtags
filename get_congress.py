455bd3a867aa8f5bce6e615a5c4385c7f326d5b0
Timothy Renner <renner.timothy@gmail.com>
2016-05-20 22:23:25 -0500

    Added full names to the congress grabbing script, updated for use with python 3.

--- get_congress.py
+++ get_congress.py
@@ -1,12 +1,12 @@
-#!/usr/bin python
-
 from sunlight import congress
 from csv import DictWriter
 import os
 
 leg = congress.all_legislators_in_office()
 
-handles = [{'party': x['party'], 'twitter': x['twitter_id']} 
+congress_names = [{'party': x['party'], 
+				   'twitter': x['twitter_id'].lower(),
+				   'name': x['first_name'] + ' ' + x['last_name']} 
 		   for x in leg if 'twitter_id' in x and
 						   x['twitter_id'] is not None]
 
@@ -16,7 +16,7 @@ if not os.path.exists(directory + "/data"):
 	os.makedirs(directory + "/data")
 
 with open('data/congress.csv','w') as f:
-	writer = DictWriter(f, fieldnames = ['party', 'twitter'])
+	writer = DictWriter(f, fieldnames = ['party', 'twitter', 'name'])
 	writer.writeheader()
-	for x in handles:
+	for x in congress_names:
 		writer.writerow(x)
--- get_congress_tweets.py
+++ get_congress_tweets.py
@@ -1,5 +1,3 @@
-#!/usr/bin python
-
 import pandas as pd
 import os
 from subprocess import call
